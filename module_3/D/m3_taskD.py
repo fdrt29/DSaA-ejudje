c0a9eead9f8466de93e88c09a7698ccee38dfe22
fdrt29 <fdrt29@gmail.com>
2021-12-09 11:37:58 +0300

    3m task D modify regex for handel zero size from user

--- module_3/D/m3_taskD.py
+++ module_3/D/m3_taskD.py
@@ -125,7 +125,7 @@ def prime_gen():
 if __name__ == '__main__':
     bl_filter = None
 
-    set_size_pattern = re.compile(r"^set ([+]?\d+)\s([+]?\d+\.\d+)\n")  # 1st group - int, 2nd - float
+    set_size_pattern = re.compile(r"^set ([+]?[1-9]\d+)\s([+]?\d+\.\d+)\n")  # 1st group - int, 2nd - float
     add_pattern = re.compile(r"^add ([^ ]+)\n")  # non-space chars in group
     search_pattern = re.compile(r"^search ([^ ]+)\n")
     print_pattern = re.compile(r"^print\n")
