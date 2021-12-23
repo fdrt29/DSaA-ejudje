f8133f901c0e7c01cc6bae02d072b2267cfda531
Teleshov Fedor <fdrt29@gmail.com>
2021-12-23 03:51:38 +0300

    incremental sieve add 2 to returned list

--- module_3/D/prime.py
+++ module_3/D/prime.py
@@ -17,30 +17,32 @@ def is_prime(prime_list, multiple_list, candidate):
 
 
 def get_prime_list(list_length):
-    if "primes" not in get_prime_list.__dict__:
-        get_prime_list.primes = [3]  # analog of a static variable of function
-        get_prime_list.multiples = [3]
+    if "primes" not in get_prime_list.__dict__:  # caching, analog of a static variable of function
+        get_prime_list.primes = [3]
+        get_prime_list.multiples = [3]  # multiple of the corresponding prime closest to the candidate
     primes = get_prime_list.primes
     multiples = get_prime_list.multiples
+    list_length -= 1  # 2 is not in the list of prime numbers
     if list_length < len(primes):
-        return primes[:list_length]
+        return [2] + primes[:list_length]
     candidate = 5
     while len(primes) < list_length:
         if is_prime(primes, multiples, candidate):
             primes.append(candidate)
             multiples.append(candidate)
         candidate += 2
-    return primes
-
-
-start = time.time()
-ls = get_prime_list(1000)
-end = time.time()
-print(end - start)
+    return [2] + primes
 
 
 start = time.time()
 ls = get_prime_list(20)
 end = time.time()
 print(end - start)
+print(ls)
+
+start = time.time()
+ls = get_prime_list(10)
+end = time.time()
+print(end - start)
+print(ls)
 # print(get_prime_list(20))
