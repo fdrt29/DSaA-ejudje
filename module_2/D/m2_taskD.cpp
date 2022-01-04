43dfe5238cd4d2f1430e4bb29bc125e6c8b562ee
fdrt29 <fdrt29@gmail.com>
2022-01-05 02:43:03 +0300

    2m task D вроде работает

--- module_2/D/m2_taskD.cpp
+++ module_2/D/m2_taskD.cpp
@@ -1,13 +1,16 @@
 #include <algorithm>
 #include <iostream>
 #include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
 #include <unordered_map>
 #include <vector>
 
 typedef int64_t key_type;
 typedef std::string val_type;
 
-size_t match_end_position(std::string what, std::string where) {
+size_t MatchEndPosition(std::string what, std::string where) {
   auto itr_what = what.begin();
   auto itr_where = where.begin();
   size_t res = 0;
@@ -48,66 +51,71 @@ class CompressedTrie {
      * Соответствие первых букв слова и метке ребра находится за
      * константу(хэш-таблица). При каждом посещении вершины необходимо
      * итерироваться по суффиксу изначального слова для определения
-     * максимального совпадающего с меткой префикса вплоть до "вырождения"
-     * суффикса в худшем случае, в остальных - его необходимо вставить.
-     * По памяти: O(1) - не зависит от входа - вставляется 1-2 узла, когда
-     * найдено место.
+     * максимального совпадающего с меткой префикса вплоть до конца
+     * суффикса в худшем случае, в остальных - остаток суффикса необходимо
+     * вставить.
+     * По памяти: O(1) - не зависит от входа - вставляется 0-2 узла,
+     * когда найдено место.
      * */
-    Node* cur = root_;
+    Node* traverse_node = root_;
 
     while (true) {
-      auto itr_edge = cur->edges.find(word[0]);
-      if (itr_edge == cur->edges.end()) {
-        // cur hasn't matching edge
+      auto itr_edge = traverse_node->edges.find(word[0]);
+      if (itr_edge == traverse_node->edges.end()) {
+        // Traverse node hasn't matching edge
         // Create new edge marked {word}
-        cur->edges[word[0]] = Edge{word, new Node(true)};
+        traverse_node->edges[word[0]] = Edge{word, new Node(true)};
         break;
       }
+
       auto& edge = itr_edge->second;
       auto label = edge.label;
-      if (word == itr_edge->second.label) {
+      // Traverse node has matching edge.
+      // 4 cases. > and < mean substr
+      // word == label
+      //      mark pointed node as end
+      // word > label
+      //      word = word[i_end:]; continue
+      // word < label
+      //      cur edges[word] = new node();
+      //      new_node.edges[label[i_end:]] = old_node;
+      //      setup old edge from cur edges
+      // have equal prefix
+      //      prefix = word[:i_end];
+      //      cur edges[prefix] = new node();
+      //      new_node.edges[label[i_end:]] = old node;
+      //      new_node.edges[word[i_end:]] = new node();
+      if (word == label) {
         edge.node_on_end->is_word_end = true;
         return;
       }
       // i_end points to next after match end
-      auto i_end = match_end_position(word, edge.label);
-      // >,< mean substr
-      // word > label
-      //      word = word[i_end:]; continue
-      // word < label
-      //      cur.edges[word] = new node();
-      //      new_node.edges[label[i_end:]] = old_node;
-      //      change old edge from cur.edges
-      // have equal prefix
-      //      prefix = word[:i_end];
-      //      cur.edges[prefix] = new node();
-      //      new_node.edges[label[i_end:]] = old node;
-      //      new_node.edges[word[i_end:]] = new node();
-      // word == label
-      //      mark pointed node as end
+      auto i_end = MatchEndPosition(word, edge.label);
+
       if (i_end == label.size() and i_end < word.size()) {
         word = word.substr(i_end);
-        cur = edge.node_on_end;
+        traverse_node = edge.node_on_end;
         continue;
 
       } else if (i_end == word.size() and i_end < label.size()) {
         // Copy old edge
-        auto old_edge = cur->edges[label[0]];
+        auto old_edge = traverse_node->edges[label[0]];
         // Create new edge
         auto new_node = new Node(true);
-        cur->edges[word[0]] = Edge{word, new_node};
+        traverse_node->edges[label[0]] = Edge{word, new_node};
         // Setup old edge
         auto new_label = label.substr(i_end);
         old_edge.label = new_label;
         // move old edge to new node
         new_node->edges[new_label[0]] = std::move(old_edge);
         return;
+
       } else if (i_end < word.size() and i_end < label.size()) {
         auto prefix = word.substr(0, i_end);
-        auto old_edge = cur->edges[label[0]];
+        auto old_edge = traverse_node->edges[label[0]];
 
         auto new_inner_node = new Node(false);
-        cur->edges[prefix[0]] = Edge{prefix, new_inner_node};
+        traverse_node->edges[label[0]] = Edge{prefix, new_inner_node};
         // Move old edge to inner node
         auto new_label = label.substr(i_end);
         old_edge.label = new_label;
@@ -120,71 +128,82 @@ class CompressedTrie {
     }
   }
 
-  bool Search(std::string word) {
-    Node* cur = root_;
+  std::set<std::string> Similar(const std::string& word,
+                                uint max_mistake_count = 1) {
+    std::set<std::string> results;
 
-    while (not word.empty()) {
-      auto itr_edge = cur->edges.find(word[0]);
-      if (itr_edge == cur->edges.end()) {
-        return false;
+    // TODO использовать очередь с приоритетом? log вставка в нее или итерация?
+    std::stack<std::pair<Node*, std::string>> stack;
+    stack.push(std::make_pair(root_, ""));
+
+    while (not stack.empty()) {
+      auto [traverse_node, path] = stack.top();
+      stack.pop();
+
+      for (auto& [ch, edge] : traverse_node->edges) {
+        auto row =
+            DamerauLevenshtein(path + edge.label, word, max_mistake_count);
+
+        if (row.back() == 0) {
+          return {path + edge.label};
+        }
+        if (row.back() == max_mistake_count and edge.node_on_end->is_word_end) {
+          results.insert(path + edge.label);
+        }
+        // If size are not equal, returned row is not last in the table.
+        // The reason is optimization inside the DamerauLevenshtein() function.
+        if (row[0] == path.length() + edge.label.length()) {
+          stack.push(std::make_pair(edge.node_on_end, path + edge.label));
+        }
       }
-      if (itr_edge->second.label == word) {
-        return itr_edge->second.node_on_end->is_word_end;
-      }
-      auto i_end = match_end_position(word, itr_edge->second.label);
-      word = word.substr(i_end);
-      cur = cur->edges[itr_edge->first].node_on_end;
     }
-    return false;
-  }
 
-  std::vector<std::string> Similar(std::string& word) {
-    std::vector<int> current_row(word.size() + 1);
-    std::iota(current_row.begin(), current_row.end(), 0);
-    std::vector<int> previous_row(word.size() + 1);
-
-    std::vector<std::string> results;
-    for (auto& [ch, edge] : root_->edges) {
-      std::string prefix = std::to_string(ch);
-      SimilarRecursive(edge.node_on_end, prefix, ch, ' ', word,
-                       current_row, previous_row, results);
-    }
     return results;
   }
 
  private:
-  void SimilarRecursive(Node* node, std::string& prefix, char ch, char prev_ch,
-                        std::string& word, std::vector<int>& previous_row,
-                        std::vector<int>& pre_previous_row,
-                        std::vector<std::string>& results) {
-    size_t columns = word.size() + 1;
-    std::vector<int> current_row = {previous_row[0] + 1};
-    for (int column = 0; column < columns; ++column) {
-      int insert_cost = previous_row[column] + 1;
-      int delete_cost = current_row[column - 1] + 1;
-      int replace_cost = previous_row[column - 1];
+  // Returns last row of Damerau-Levenshtein table or last row that have value
+  // <=1.
+  static std::vector<uint> DamerauLevenshtein(const std::string& str1,
+                                              const std::string& str2,
+                                              uint max_mistake_count = 1) {
+    auto n = str1.length() + 1;
+    auto m = str2.length() + 1;
 
-      if (word[column - 1] != ch) replace_cost += 1;
-      current_row.push_back(
-          std::min(std::min(insert_cost, delete_cost), replace_cost));
-      if (prev_ch != ' ' and column > 1 and ch == word[column - 2] and
-          prev_ch == word[column - 1] and word[column - 1] != ch) {
-        current_row[column] =
-            std::min(current_row[column], pre_previous_row[column - 2] + 1);
-      }
-    }
-    if (current_row[-1] == 1 and node->is_word_end) {
-      results.push_back(prefix);
-    }
-    if (*std::min_element(current_row.begin(), current_row.end()) <= 1) {
-      prev_ch = ch;  // TODO mb отдельная переменная
+    std::vector<uint> pre_previous(m);
 
-      for (auto& [sym, child] : node->edges) {
-        std::string new_pref = prefix + sym;
-        SimilarRecursive(child.node_on_end, new_pref, sym, prev_ch, word,
-                         current_row, previous_row, results);
+    std::vector<uint> previous(m);
+    std::iota(previous.begin(), previous.end(), 0);
+
+    for (auto i = 1; i < n; i++) {
+      std::vector<uint> current(m);
+      current[0] = i;
+      bool curr_has_valid_mistakes_count = false;
+      for (auto j = 1; j < m; j++) {
+        uint cost = str1[i - 1] == str2[j - 1] ? 0 : 1;
+
+        uint insert = previous[j] + 1;
+        uint del = current[j - 1] + 1;
+        uint replace = previous[j - 1] + cost;
+
+        current[j] = std::min({insert, del, replace});
+
+        if (i > 1 && j > 1 && str1[i - 1] == str2[j - 2] &&
+            str1[i - 2] == str2[j - 1]) {
+          // transposition
+          current[j] = std::min(current[j], pre_previous[j - 2] + cost);
+        }
+        if (not curr_has_valid_mistakes_count and
+            current[j] <= max_mistake_count)
+          curr_has_valid_mistakes_count = true;
       }
+      pre_previous = previous;
+      previous = current;
+      // Optimisation.
+      if (not curr_has_valid_mistakes_count) return previous;
     }
+
+    return previous;
   }
 
  private:
@@ -195,14 +214,17 @@ class CompressedTrie {
 //
 // ----------- Text Interface --------------------------------------------------
 
-void InteractWithDSByTextCommands(std::istream& in, std::ostream& out) {
+void InteractWithTextCommands(std::istream& in, std::ostream& out) {
   CompressedTrie trie{};
   std::string line;
 
   key_t n = 0;
   std::cin >> n;
+  std::cin.ignore();
   for (int i = 0; i < n; ++i) {
     std::getline(std::cin, line);
+    std::transform(line.begin(), line.end(), line.begin(),
+                   [](unsigned char c) { return std::tolower(c); });
     trie.Insert(line);
   }
 
@@ -211,15 +233,14 @@ void InteractWithDSByTextCommands(std::istream& in, std::ostream& out) {
 
     std::transform(line.begin(), line.end(), line.begin(),
                    [](unsigned char c) { return std::tolower(c); });
-    std::vector<std::string> res = trie.Similar(line);
+    std::set<std::string> res = trie.Similar(line);
     if (res.empty()) {
-      std::cout << line << " -?" << std::endl;
+      std::cout << line << " - ?" << std::endl;
       continue;
     }
-    if (std::find(res.begin(), res.end(), line) != res.end()) {
-      std::cout << line << " -ok" << std::endl;
+    if (res.find(line) != res.end()) {
+      std::cout << line << " - ok" << std::endl;
     } else {
-      std::sort(res.begin(), res.end());
       std::cout << line << " -> ";
       auto i = 0;
       for (const auto& w : res) {
@@ -233,11 +254,6 @@ void InteractWithDSByTextCommands(std::istream& in, std::ostream& out) {
 }
 
 int main() {
-  //  CompressedTrie trie;
-  //  trie.Insert("thereafter");
-  //  trie.Insert("therein");
-  //  trie.Insert("this");
-  //  trie.Search("therei");
-  InteractWithDSByTextCommands(std::cin, std::cout);
+  InteractWithTextCommands(std::cin, std::cout);
   return 0;
 }
