634ab5a757b0b3f7249f2acd0751b8fd42b1b09d
fdrt29 <fdrt29@gmail.com>
2022-01-08 02:32:22 +0300

    2m task D recursive implementation

--- module_2/D/m2_taskD.cpp
+++ module_2/D/m2_taskD.cpp
@@ -3,8 +3,8 @@
 #include <iostream>
 #include <numeric>
 #include <set>
-#include <stack>
 #include <unordered_map>
+#include <utility>
 #include <vector>
 
 size_t MatchEndPosition(std::wstring what, std::wstring where) {
@@ -21,53 +21,55 @@ size_t MatchEndPosition(std::wstring what, std::wstring where) {
 }
 
 class RadixTrie {
-  struct Node;
-  struct Edge {
-    std::wstring label;
-    Node* node_on_end;
-  };
   struct Node {
-    explicit Node(bool word_end) : is_word_end(word_end), edges() {}
     bool is_word_end = false;
-    // Store as key first char, whole label stored in value with ptr
-    std::unordered_map<wchar_t, Edge> edges;
+    std::unordered_map<wchar_t, Node*> children;
+    std::wstring label;
+
+    explicit Node(std::wstring label = std::wstring(), bool word_end = false)
+        : label(std::move(label)), is_word_end(word_end), children() {}
 
     ~Node() {
-      for (auto const& el : edges) {
-        delete el.second.node_on_end;
+      for (auto const& el : children) {
+        delete el.second;
       }
     }
   };
 
  public:
-  ~RadixTrie() { delete root_; }
+  ~RadixTrie() {
+    delete root_;
+  }
 
  public:
   /* Вставка.
-   * Сложность по времени О(|word|), |word|- длина слова.
+   * Сложность по времени О(l), l - длина слова.
    * Соответствие первых букв слова и метке ребра находится за
    * константу(хэш-таблица). При каждом посещении вершины необходимо
    * итерироваться по суффиксу изначального слова для определения
    * максимального совпадающего с меткой префикса вплоть до конца
-   * суффикса в худшем случае, в остальных - остаток суффикса необходимо
+   * суффикса(переключаясь на дочерний узел если метка текущего узла - подстрока
+   * суффикса) в худшем случае, в остальных - остаток суффикса просто необходимо
    * вставить.
-   * По памяти: O(1) - не зависит от входа. Вставляется 0-2 узла,
-   * когда найдено место.
+   *
+   * По памяти: O(1) - не зависит от входа.
+   *
+   * Вставляется 0-2 узла, когда найдено место.
    * */
   void Insert(std::wstring word) {
     Node* traverse_node = root_;
 
     while (true) {
-      auto itr_edge = traverse_node->edges.find(word[0]);
-      if (itr_edge == traverse_node->edges.end()) {
-        // Traverse node hasn't matching edge
-        // Create new edge marked {word}
-        traverse_node->edges[word[0]] = Edge{word, new Node(true)};
+      auto itr_edge = traverse_node->children.find(word[0]);
+      if (itr_edge == traverse_node->children.end()) {
+        // Traverse node hasn't matching node
+        // Create new node marked {word}
+        traverse_node->children[word[0]] = new Node(word, true);
         break;
       }
 
-      auto& edge = itr_edge->second;
-      auto label = edge.label;
+      auto& p_node = itr_edge->second;
+      const auto& label = p_node->label;
       // Traverse node has matching edge.
       // 4 cases. > and < mean substr
       // word == label
@@ -84,124 +86,106 @@ class RadixTrie {
       //      new_node.edges[label[i_end:]] = old node;
       //      new_node.edges[word[i_end:]] = new node();
       if (word == label) {
-        edge.node_on_end->is_word_end = true;
+        p_node->is_word_end = true;
         return;
       }
       // i_end points to next after match end
-      auto i_end = MatchEndPosition(word, edge.label);
+      auto i_end = MatchEndPosition(word, p_node->label);
 
       if (i_end == label.size() and i_end < word.size()) {
         word = word.substr(i_end);
-        traverse_node = edge.node_on_end;
+        traverse_node = p_node;
         continue;
 
       } else if (i_end == word.size() and i_end < label.size()) {
-        // Copy old edge
-        auto old_edge = traverse_node->edges[label[0]];
-        // Create new edge
-        auto new_node = new Node(true);
-        traverse_node->edges[label[0]] = Edge{word, new_node};
-        // Setup old edge
+        // Copy old node
+        auto old_node = traverse_node->children[label[0]];
+        // Create new node
+        auto new_node = new Node(word, true);
+        traverse_node->children[label[0]] = new_node;
+        // Setup old node
         auto new_label = label.substr(i_end);
-        old_edge.label = new_label;
-        // move old edge to new node
-        new_node->edges[new_label[0]] = std::move(old_edge);
+        old_node->label = new_label;
+        // move old node to new node
+        new_node->children[new_label[0]] = old_node;
         return;
 
       } else if (i_end < word.size() and i_end < label.size()) {
         auto prefix = word.substr(0, i_end);
-        auto old_edge = traverse_node->edges[label[0]];
+        auto old_node = traverse_node->children[label[0]];
 
-        auto new_inner_node = new Node(false);
-        traverse_node->edges[label[0]] = Edge{prefix, new_inner_node};
-        // Move old edge to inner node
-        auto new_label = label.substr(i_end);
-        old_edge.label = new_label;
-        new_inner_node->edges[label[i_end]] = std::move(old_edge);
-        // Create new edge from inner node to new node
-        new_inner_node->edges[word[i_end]] =
-            Edge{word.substr(i_end), new Node(true)};
+        auto new_inner_node = new Node(prefix, false);
+        traverse_node->children[label[0]] = new_inner_node;
+        // Move old node to inner node
+        old_node->label = label.substr(i_end);
+        new_inner_node->children[old_node->label[0]] = old_node;
+        // Create new node from inner node to new node
+        new_inner_node->children[word[i_end]] =
+            new Node(word.substr(i_end), true);
         return;
       }
     }
   }
 
   /* Нечеткий поиск.
-   * Сложность по времени: О(k*n), k - длина искомого слова, n - число узлов в
-   * Radix Trie. Для каждого узла, имеющего расстояние 1 хотя бы между 2
-   * префиксами искомого слова и метки, проверяем всех его детей. В худшем
-   * случае придется пройти по всем узлам: каждый узел будет содержать только
-   * одну букву искомого слова и иметь ребенка, удовлетворяющего названному выше
-   * условию. Для каждого узла будет рассчитано расстояние Дамерау-Левенштейна
-   * за О(k*С). С - константа.
+   * По времени: О(n*l), где n - количество узлов(или количество символов
+   * хранимых деревом если точнее) в дереве, l - длина искомого слова. Всего
+   * создается строк в таблице не больше, чем символов во всех проверенных
+   * префиксном пути. Длина строк - l. В худшем случае придется пройти по всем
+   * узлам и символам. Худший случай: расстояние всех итоговых путей в дереве до
+   * искомого слова равно 1.
    *
-   * Сложность по памяти: О(n+k). О(k) для Дамерау-Левенштейна + память на стек
-   * O(n), в который поместятся все узлы в худшем случае: корень имеет n ребер,
-   * каждое из которых подходит.
+   * По памяти: О((l+1)*l)=O(l^2)=O(n*l). Для каждого узла будет храниться 3
+   * строки таблицы длиной l + доп память на фрейм. Худший случай: l+1 узлов
+   * уходят в глубину и каждый равен соответствующей букве слова, последний -
+   * конец слова с дополнительной буквой относительно искомого(на каждом уровне
+   * имеются какие-то другие дети, чтобы путь был не сжатым). Стек вызовов будет
+   * содержать l+1 фреймов рекурсивной функции, каждый будет хранить 3 строки
+   * длинной l.
    * */
-  /// @return Exact match or otherwise similar words
   std::set<std::wstring> FuzzySearch(const std::wstring& word,
                                      uint max_mistake_count = 1) {
     std::set<std::wstring> results;
 
-    // TODO использовать очередь с приоритетом? log вставка в нее или итерация?
-    std::stack<std::pair<Node*, std::wstring>> stack;
-    stack.push(std::make_pair(root_, L""));
+    auto m = word.length() + 1;
+    std::vector<uint> pre_previous(m);
+    std::vector<uint> previous(m);
+    std::iota(previous.begin(), previous.end(), 0);
 
-    while (not stack.empty()) {
-      auto [traverse_node, path] = stack.top();
-      stack.pop();
-      // Traverse node have valid distance, check its children
-      for (auto& [ch, edge] : traverse_node->edges) {
-        auto current_path = path + edge.label;
-        auto o_dist =
-            TryDamerauLevenshtein(current_path, word, max_mistake_count);
-
-        // If not true, returned row is not last in the table.
-        // The reason is optimization inside the DamerauLevenshtein() function.
-        if (o_dist.has_value()) {
-          if (o_dist.value() == 0 and edge.node_on_end->is_word_end) {
-            return {current_path};
-          }
-          if (o_dist.value() == max_mistake_count and
-              edge.node_on_end->is_word_end) {
-            results.insert(current_path);
-          }
-
-          stack.push(std::make_pair(edge.node_on_end, current_path));
-        }
-      }
+    for (auto& [ch, p_node] : root_->children) {
+      RecursiveFuzzySearch(p_node, pre_previous, previous, results,
+                           root_->label, word, max_mistake_count);
     }
 
     return results;
   }
 
  private:
-  /* Расстояние Дамерау-Левенштейна.
-   * Сложность по времени: O(k*l). k на инициализацию, k*l итераций по таблице
-   * в худшем случае. k - длина искомого слова, l - длина метки ребра
-   *
-   * Сложность по памяти: О(3(k+1))=O(k). Храним 3 строки
-   * таблицы, длина которых ~ длине k искомого слова.
-   * */
-  static std::optional<uint> TryDamerauLevenshtein(const std::wstring& str1,
-                                                   const std::wstring& str2,
-                                                   uint max_mistake_count = 1) {
-    auto n = str1.length() + 1;
-    auto m = str2.length() + 1;
+  static void RecursiveFuzzySearch(Node* p_current_node,
+                                   std::vector<uint> pre_previous,
+                                   std::vector<uint> previous,
+                                   std::set<std::wstring>& results,
+                                   const std::wstring& prefix,
+                                   const std::wstring& word,
+                                   uint max_mistake_count = 1) {
+    std::wstring current_labels_path = prefix + p_current_node->label;
 
-    std::vector<uint> pre_previous(m);
-
-    std::vector<uint> previous(m);
-    std::iota(previous.begin(), previous.end(), 0);
+    // Similar word and input word lengths may differ by no more than the
+    // max mistake count. Because a possible mistake is inserting an extra
+    // character
+    int length_difference = static_cast<int>(current_labels_path.length()) -
+                            static_cast<int>(word.length());
+    if (length_difference > static_cast<int>(max_mistake_count)) return;
 
     bool prev_has_valid_mistakes_count = false;
-    for (auto i = 1; i < n; i++) {
-      std::vector<uint> current(m);
+    // Достраиваем таблицу
+    for (auto i = prefix.size() + 1; i < current_labels_path.length() + 1;
+         i++) {
+      std::vector<uint> current(word.length() + 1);
       current[0] = i;
       bool curr_has_valid_mistakes_count = false;
-      for (auto j = 1; j < m; j++) {
-        uint cost = str1[i - 1] == str2[j - 1] ? 0 : 1;
+      for (auto j = 1; j < word.length() + 1; j++) {
+        uint cost = current_labels_path[i - 1] == word[j - 1] ? 0 : 1;
 
         uint insert = previous[j] + 1;
         uint del = current[j - 1] + 1;
@@ -209,8 +193,8 @@ class RadixTrie {
 
         current[j] = std::min({insert, del, replace});
 
-        if (i > 1 && j > 1 && str1[i - 1] == str2[j - 2] &&
-            str1[i - 2] == str2[j - 1]) {
+        if (i > 1 && j > 1 && current_labels_path[i - 1] == word[j - 2] &&
+            current_labels_path[i - 2] == word[j - 1]) {
           // Transposition
           current[j] = std::min(current[j], pre_previous[j - 2] + cost);
         }
@@ -218,20 +202,26 @@ class RadixTrie {
             current[j] <= max_mistake_count)
           curr_has_valid_mistakes_count = true;
       }
-      // Optimisation.
       if (not curr_has_valid_mistakes_count and prev_has_valid_mistakes_count)
-        return std::nullopt;  // Early termination of algorithm
+        return;  // Early termination of algorithm
 
       pre_previous = previous;
       previous = current;
       prev_has_valid_mistakes_count = curr_has_valid_mistakes_count;
     }
 
-    return previous.back();
+    if (previous.back() <= max_mistake_count and p_current_node->is_word_end) {
+      results.insert(current_labels_path);
+    }
+
+    for (auto& [ch, p_node] : p_current_node->children) {
+      RecursiveFuzzySearch(p_node, pre_previous, previous, results,
+                           current_labels_path, word, max_mistake_count);
+    }
   }
 
  private:
-  Node* root_ = new Node(false);
+  Node* root_ = new Node();
 };
 
 //
